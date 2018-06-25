06238708ac60aef2f2354fda022abedb88de0820
root <makurov@gmail.com>
2018-06-25 15:25:38 +0500

    feat: prototype nmap support for accessiblity checks

--- src/libs/zbxicmpping/icmpping.c
+++ src/libs/zbxicmpping/icmpping.c
@@ -22,6 +22,8 @@
 #include "comms.h"
 #include "log.h"
 
+#define MAX_ICMP_NMAP_FIELDS		10
+
 extern char	*CONFIG_SOURCE_IP;
 extern char	*CONFIG_FPING_LOCATION;
 #ifdef HAVE_IPV6
@@ -76,11 +78,14 @@ static void	get_source_ip_option(const char *fping, const char **option, unsigne
 static int	process_ping(ZBX_FPING_HOST *hosts, int hosts_count, int count, int interval, int size, int timeout,
 		char *error, int max_error_len)
 {
+
+
 	const char	*__function_name = "process_ping";
+
 
 	FILE		*f;
 	char		*c, params[64];
-	char		filename[MAX_STRING_LEN], tmp[MAX_STRING_LEN];
+	char		filename[MAX_STRING_LEN], tmp[MAX_STRING_LEN], nmap_tmp[MAX_STRING_LEN];
 	size_t		offset;
 	ZBX_FPING_HOST	*host;
 	double		sec;
@@ -222,7 +227,7 @@ static int	process_ping(ZBX_FPING_HOST *hosts, int hosts_count, int count, int i
 			zbx_snprintf(tmp + offset, sizeof(tmp) - offset,
 					"%s %s 2>&1 <%s;", CONFIG_FPING6_LOCATION, params6, filename);
 	}
-#else
+#elsel
 	zbx_snprintf(tmp, sizeof(tmp), "%s %s 2>&1 <%s", CONFIG_FPING_LOCATION, params, filename);
 #endif	/* HAVE_IPV6 */
 
@@ -244,119 +249,246 @@ static int	process_ping(ZBX_FPING_HOST *hosts, int hosts_count, int count, int i
 
 	zabbix_log(LOG_LEVEL_DEBUG, "%s", tmp);
 
-	if (NULL == (f = popen(tmp, "r")))
-	{
-		zbx_snprintf(error, max_error_len, "%s: %s", tmp, zbx_strerror(errno));
+	if (1==count) {
+    
+		zbx_snprintf(nmap_tmp, sizeof(nmap_tmp), "%s %s 2>&1 <%s", "/usr/bin/nmap", "-sP -T5 -iL", filename);
+		zabbix_log(LOG_LEVEL_DEBUG, "will do %s", nmap_tmp);
 
-		unlink(filename);
+		//if count set to one, then we'll use nmap as the utility
+		//its way faster
+		//but it ouputs in diffent format, so we need kind an adaptation 
+		//example of nmap output 
+		//Nmap scan report for bs89594.is74.ru (10.102.159.245)
+		//Host is up (0.0012s latency).
+		//rDNS record for 10.102.159.245: bs86447.is74.ru
+		//Nmap scan report for bs14419.is74.ru (10.102.151.189)
+		//Host is up (0.0012s latency).
+		//Nmap scan report for bs11954.is74.ru (10.102.178.1)
+		//Host is up (0.0030s latency).
+		//Nmap scan report for bs85530.is74.ru (10.102.207.56)
+		//Host is up (0.0010s latency).
 
-		return ret;
-	}
+		//fping output for :
 
-	if (NULL == fgets(tmp, sizeof(tmp), f))
-	{
-		strscpy(tmp, "no output");
-	}
-	else
-	{
-		for (i = 0; i < hosts_count; i++)
+		//bs15558.is74.ru : 1.14 1.16 1.17 1.20 1.16
+		//bs20623.is74.ru : 5.62 5.35 5.50 5.43 5.56
+		//bs54917.is74.ru : 4.33 3.92 3.94 3.91 3.97
+		//bs05184.is74.ru : 1.44 1.51 1.46 1.34 1.31
+		//bs67420.is74.ru : 2.32 2.33 2.32 2.51 2.34
+		//bs20513.is74.ru : 4.25 4.24 4.29 5.40 4.39
+
+		if (NULL == (f = popen(nmap_tmp, "r")))
 		{
-			hosts[i].status = zbx_malloc(NULL, count);
-			memset(hosts[i].status, 0, count);
+			zbx_snprintf(error, max_error_len, "%s: %s", tmp, zbx_strerror(errno));
+
+			unlink(filename);
+
+			return ret;
 		}
 
+		if (NULL == fgets(tmp, sizeof(tmp), f))
+			return ret;
+		
 		do
 		{
 			zbx_rtrim(tmp, "\n");
 			zabbix_log(LOG_LEVEL_DEBUG, "read line [%s]", tmp);
 
-			host = NULL;
 
-			if (NULL != (c = strchr(tmp, ' ')))
+			char *fields[MAX_ICMP_NMAP_FIELDS];
+			char *end_field;
+
+
+			//splitting line into fields
+		 
+			char *field_ptr = strtok_r(tmp, " ", &end_field);
+			int field_count=0;
+
+			for (i=0; i++; i<field_count) 
+			fields[i]=NULL; 
+
+			while (field_ptr != NULL && MAX_ICMP_NMAP_FIELDS>field_count) 
+			{	
+
+			    fields[field_count++]=field_ptr;
+			    field_ptr = strtok_r(NULL, " ", &end_field);
+			}
+
+			if (NULL == fields[4])
+				 continue; 
+			//we've got at least 5 fields, checking if this line starts from Nmap ..
+			if ( 	strcmp("Nmap",fields[0]) != 0 ||
+				strcmp("scan",fields[1]) !=0  ||
+				strcmp("for", fields[2]) !=0 )   continue;
+
+
+			host = NULL;
+			//looking for the first space
+			for (i = 0; i < hosts_count; i++)
 			{
-				*c = '\0';
-				for (i = 0; i < hosts_count; i++)
-					if (0 == strcmp(tmp, hosts[i].addr))
-					{
-						host = &hosts[i];
-						break;
-					}
-				*c = ' ';
+				if (0 == strcmp(fields[4], hosts[i].addr))
+				{
+					host = &hosts[i];
+					break;
+				}
 			}
 
 			if (NULL == host)
 				continue;
 
-			if (NULL == (c = strstr(tmp, " : ")))
+			//got the host, reading and splitting the next line 
+			char *latency;
+			if (NULL == fgets(tmp, sizeof(tmp), f)) 
 				continue;
+				
+			field_ptr = strtok_r(tmp, " ", &end_field);
+				
+			for (i = 0; i++; i<field_count)
+				fields[i]=NULL;
 
-			/* when NIC bonding is used, there are also lines like */
-			/* 192.168.1.2 : duplicate for [0], 96 bytes, 0.19 ms */
-
-			if (NULL != strstr(tmp, "duplicate for"))
-				continue;
-
-			c += 3;
-
-			/* The were two issues with processing only the fping's final status line:  */
-			/*   1) pinging broadcast addresses could have resulted in responses from   */
-			/*      different hosts, which were counted as the target host responses;   */
-			/*   2) there is a bug in fping (v3.8 at least) where pinging broadcast     */
-			/*      address will result in no individual responses, but the final       */
-			/*      status line might contain a bogus value.                            */
-			/* Because of the above issues we must monitor the individual responses     */
-			/* and mark the valid ones.                                                 */
-			if ('[' == *c)
-			{
-				/* Fping appends response source address in format '[<- 10.3.0.10]' */
-				/* if it does not match the target address. Ignore such responses.  */
-				if (NULL != strstr(c + 1, "[<-"))
-					continue;
-
-				/* get the index of individual ping response */
-				index = atoi(c + 1);
-
-				if (0 > index || index >= count)
-					continue;
-
-				host->status[index] = 1;
-
-				continue;
+			while (field_ptr != NULL && MAX_ICMP_NMAP_FIELDS>field_count) 
+			{	
+				fields[field_count++]=field_ptr;
+				field_ptr = strtok_r(NULL, " ", &end_field);
 			}
 
-			/* process status line for a host */
-			index = 0;
-			do
-			{
-				if (1 == host->status[index])
-				{
-					sec = atof(c) / 1000; /* convert ms to seconds */
+			if (NULL == fields[3])
+				 continue; 
 
-					if (0 == host->rcv || host->min > sec)
-						host->min = sec;
-					if (0 == host->rcv || host->max < sec)
-						host->max = sec;
-					host->sum += sec;
-					host->rcv++;
-				}
-			}
-			while (++index < count && NULL != (c = strchr(c + 1, ' ')));
+			//we've got at least 5 fields, checking if this line starts from Nmap ..
+			if ( 	strcmp("Host",	fields[0]) != 0 ||
+				strcmp("is",	fields[1]) !=0  ||
+				strcmp("up",	fields[2]) !=0 )   
+				continue;
 
-			host->cnt += count;
-#ifdef HAVE_IPV6
-			if (host->cnt == count && NULL == CONFIG_SOURCE_IP &&
-					0 != (fping_existence & FPING_EXISTS) &&
-					0 != (fping_existence & FPING6_EXISTS))
-			{
-				memset(host->status, 0, count);	/* reset response statuses for IPv6 */
-			}
-#endif
-			ret = SUCCEED;
+			latency=fields[3]+1;
+			zbx_rtrim(latency,"s");
+				
+			sec=atof(latency);
+			host->rcv=1;
+			host->min=sec;
+			host->max=sec;
+			host->sum=sec;
+			zabbix_log(LOG_LEVEL_DEBUG, "Final parced info is host=%s , latency=%f",host->addr,sec);
 		}
 		while (NULL != fgets(tmp, sizeof(tmp), f));
 
-		for (i = 0; i < hosts_count; i++)
-			zbx_free(hosts[i].status);
+		
+	} else 
+	{
+		if (NULL == (f = popen(tmp, "r")))
+		{
+			zbx_snprintf(error, max_error_len, "%s: %s", tmp, zbx_strerror(errno));
+
+			unlink(filename);
+
+			return ret;
+		}
+
+		if (NULL == fgets(tmp, sizeof(tmp), f))
+		{
+			strscpy(tmp, "no output");
+		}
+		else
+		{
+			for (i = 0; i < hosts_count; i++)
+			{
+				hosts[i].status = zbx_malloc(NULL, count);
+				memset(hosts[i].status, 0, count);
+			}
+
+			do
+			{
+				zbx_rtrim(tmp, "\n");
+				zabbix_log(LOG_LEVEL_DEBUG, "read line [%s]", tmp);
+
+				host = NULL;
+
+				if (NULL != (c = strchr(tmp, ' ')))
+				{
+					*c = '\0';
+					for (i = 0; i < hosts_count; i++)
+						if (0 == strcmp(tmp, hosts[i].addr))
+						{
+							host = &hosts[i];
+							break;
+						}
+					*c = ' ';
+				}
+
+				if (NULL == host)
+					continue;
+
+				if (NULL == (c = strstr(tmp, " : ")))
+					continue;
+
+				/* when NIC bonding is used, there are also lines like */
+				/* 192.168.1.2 : duplicate for [0], 96 bytes, 0.19 ms */
+
+				if (NULL != strstr(tmp, "duplicate for"))
+					continue;
+
+				c += 3;
+
+				/* The were two issues with processing only the fping's final status line:  */
+				/*   1) pinging broadcast addresses could have resulted in responses from   */
+				/*      different hosts, which were counted as the target host responses;   */
+				/*   2) there is a bug in fping (v3.8 at least) where pinging broadcast     */
+				/*      address will result in no individual responses, but the final       */
+				/*      status line might contain a bogus value.                            */
+				/* Because of the above issues we must monitor the individual responses     */
+				/* and mark the valid ones.                                                 */
+				if ('[' == *c)
+				{
+					/* Fping appends response source address in format '[<- 10.3.0.10]' */
+					/* if it does not match the target address. Ignore such responses.  */
+					if (NULL != strstr(c + 1, "[<-"))
+						continue;
+
+					/* get the index of individual ping response */
+					index = atoi(c + 1);
+
+					if (0 > index || index >= count)
+						continue;
+
+					host->status[index] = 1;
+
+					continue;
+				}
+
+				/* process status line for a host */
+				index = 0;
+				do
+				{
+					if (1 == host->status[index])
+					{
+						sec = atof(c) / 1000; /* convert ms to seconds */
+
+						if (0 == host->rcv || host->min > sec)
+							host->min = sec;
+						if (0 == host->rcv || host->max < sec)
+							host->max = sec;
+						host->sum += sec;
+						host	->rcv++;
+					}
+				}
+				while (++index < count && NULL != (c = strchr(c + 1, ' ')));
+					host->cnt += count;
+#ifdef HAVE_IPV6
+				if (host->cnt == count && NULL == CONFIG_SOURCE_IP &&
+						0 != (fping_existence & FPING_EXISTS) &&
+						0 != (fping_existence & FPING6_EXISTS))
+				{
+					memset(host->status, 0, count);	/* reset response statuses for IPv6 */
+				}
+#endif
+				ret = SUCCEED;
+			}
+			while (NULL != fgets(tmp, sizeof(tmp), f));
+
+			for (i = 0; i < hosts_count; i++)
+				zbx_free(hosts[i].status);
+		}
 	}
 	pclose(f);
 
